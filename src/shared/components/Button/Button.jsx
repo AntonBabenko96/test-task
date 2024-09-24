f48d53c5703e549f79315e1830412b27235a97e8
Anton Babenko <116025070+AntonBabenko96@users.noreply.github.com>
2024-09-24 21:06:00 +0300

    fix btn

--- src/shared/components/Button/Button.jsx
+++ src/shared/components/Button/Button.jsx
@@ -2,7 +2,11 @@ import styles from "./Button.module.scss";
 
 const Button = ({ children, onClick, className = "" }) => {
   return (
-    <button className={`${styles.btn} ${className}`} onClick={onClick}>
+    <button
+      className={`${styles.btn} ${className}`}
+      onClick={onClick}
+      type="button"
+    >
       {children}
     </button>
   );
